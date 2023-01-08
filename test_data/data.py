3c1ed950dc97091c92bd170616add43678bda0f1
Pishone <89649425+Pishone@users.noreply.github.com>
2023-01-08 07:48:58 +0000

    update data.py to run with test_project3.py

--- test_data/data.py
+++ test_data/data.py
@@ -56,3 +56,43 @@ class element:
     delete_success_message_path = "//div[@class='oxd-toast-content oxd-toast-content--success']"
 
     
+    ## Menu Options (It's all relative!)
+    admin_menu_path = "//li[1]//a[1]//span[1]"
+    pim_menu_path = "//span[normalize-space()='PIM']"
+    leave_menu_path = "/html[1]/body[1]/div[1]/div[1]/div[1]/aside[1]/nav[1]/div[2]/ul[1]/li[3]/a[1]/span[1]"
+    time_menu_path = "//span[normalize-space()='Time']"
+    recruitment_menu_path = "//span[normalize-space()='Recruitment']"
+    myinfo_menu_path = "//span[normalize-space()='My Info']"
+    performance_menu_path = "/html[1]/body[1]/div[1]/div[1]/div[1]/aside[1]/nav[1]/div[2]/ul[1]/li[7]/a[1]/span[1]"
+    dashboard_menu_path = "//span[normalize-space()='Dashboard']"
+    directory_menu_path = "//span[normalize-space()='Directory']"
+    maintenance_menu_path = "//span[normalize-space()='Maintenance']"
+    buzz_menu_path = "//span[normalize-space()='Buzz']"
+
+    search_box_path = "//input[@placeholder='Search']"
+
+    user_management_path = "//span[normalize-space()='User Management']"
+    users_path = "//ul[@class='oxd-dropdown-menu']//li"
+    user_role_select_path = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]"
+    user_role_admin_path = "//div[@class='oxd-select-text-input'][normalize-space()='Admin']"
+
+    create_login_details_toggle_path = "//span[@class='oxd-switch-input oxd-switch-input--active --label-right']"
+    enabled_radio_button_path = "//input[@value='1']"
+
+    username_text_box_path = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[3]/div[1]/div[1]/div[1]/div[2]/input[1]"
+    password_text_box_path = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[2]/input[1]"
+    confirm_password_path = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[2]/div[4]/div[1]/div[2]/div[1]/div[2]/input[1]"
+    
+    personal_details = "//h6[normalize-space()='Personal Details']"
+
+    personal_details_menu = "/html[1]/body[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/a[1]"
+    contact_details_menu = "//a[normalize-space()='Contact Details']"
+    emergency_contacts_menu = "//a[normalize-space()='Emergency Contacts']"
+    dependents_menu = "//a[normalize-space()='Dependents']"
+    immigration_menu = "//a[normalize-space()='Immigration']"
+    job_menu = "//a[normalize-space()='Job']"
+    salary_menu = "//a[normalize-space()='Salary']"
+    tax_excemptions_menu = "//a[normalize-space()='Tax Exemptions']"
+    report_to_menu = "//a[normalize-space()='Report-to']"
+    qualifications_menu = "//a[normalize-space()='Qualifications']"
+    memberships_menu = "//a[normalize-space()='Memberships']"    
